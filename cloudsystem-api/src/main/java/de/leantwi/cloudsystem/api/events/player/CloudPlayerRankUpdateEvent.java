83bcb1d06c1e2d2f36460c882d3019acd5a64dfb
Lentry <lentry@lentry.de>
2022-09-09 23:50:36 +0200

    update

--- cloudsystem-api/src/main/java/de/leantwi/cloudsystem/api/events/player/CloudPlayerRankUpdateEvent.java
+++ cloudsystem-api/src/main/java/de/leantwi/cloudsystem/api/events/player/CloudPlayerRankUpdateEvent.java
@@ -1,10 +1,18 @@
 package de.leantwi.cloudsystem.api.events.player;
 
 import de.leantwi.cloudsystem.api.event.Event;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.UUID;
 
 @Deprecated
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CloudPlayerRankUpdateEvent extends Event {
 
     private UUID uniqueID;
